086fdfec549e34d090cc2bf23dc2be4f67b4795b
Anirudh Joshi <vuddawaranirudhjoshi03@gmail.com>
2024-08-15 21:33:37 +0530

    added env variables

--- app/components/InputPage.tsx
+++ app/components/InputPage.tsx
@@ -16,12 +16,11 @@ const InputPage: React.FC = () => {
     const checkUrl = async () => {
         setLoading(true);
         try {
-            const res = await axios.post('http://localhost:3000/api/check', { url });
+            const res = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/check`, { url });
             setValid(res.data.valid);
         } catch (error) {
             setValid(false);
         }
-        console.log(`valid:${valid}`)
         setLoading(false);
     };
 
@@ -32,29 +31,25 @@ const InputPage: React.FC = () => {
         setCustomUrl(e.target.value);
     };
     const checkCustomUrl = async () => {
-        const res = await axios.post("http://localhost:3000/api/urlCheck", {
+        const res = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/urlCheck`, {
             customUrl: customUrl
-        })
-        console.log("here")
-        setChecked(true)
-        setValidCustom(res.data.check)
+        });
+        setChecked(true);
+        setValidCustom(res.data.check);
+    };
+   const addUrlMap = async () => {
+    const res = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/addMapping`, {
+        realUrl: url,
+        userUrl: customUrl
+    });
+    if (res.data.done) {
+        alert(`Successfully added your URL ${customUrl}`);
+    } else {
+        alert("The given URL already exists");
     }
-    const addUrlMap = async () => {
-        const res = await axios.post("http://localhost:3000/api/addMapping", {
-            realUrl: url,
-            userUrl: customUrl
-        })
-        if (res.data.done) {
-            alert("Successfully added your url " + customUrl)
-        }
-        else {
-            alert("the given url already exists")
-
-        }
-        alert(`you can access your site at http://localhost:3000/custom/${customUrl}`)
-        window.location.reload();
-
-    }
+    alert(`You can access your site at ${process.env.NEXT_PUBLIC_API_BASE_URL}/custom/${customUrl}`);
+    window.location.reload();
+};
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>URL Shortener</h1>
