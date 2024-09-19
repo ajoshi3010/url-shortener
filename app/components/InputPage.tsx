5022f545b3699604275bf6472ac748fc655233c4
Anirudh Joshi <vuddawaranirudhjoshi03@gmail.com>
2024-09-19 13:02:08 +0530

    added loader wherever needed

--- app/components/InputPage.tsx
+++ app/components/InputPage.tsx
@@ -53,6 +53,7 @@ const InputPage: React.FC = () => {
     const [valid, setValid] = useState<boolean>(false);
     const [validCustom, setValidCustom] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(false);
+    const [loading2, setLoading2] = useState<boolean>(false);
     const [checked, setChecked] = useState(false);
     const [mapped, setMapped] = useState(false);
     const [isCopied, setIsCopied] = useState(false);
@@ -114,7 +115,7 @@ const InputPage: React.FC = () => {
         setValid(false);
         setChecked(false);
     };
-    const checkForSlash = (str:string): void => {
+    const checkForSlash = (str: string): void => {
         // Updated regex to require at least one character (no slashes allowed)
         const regex = /^[a-zA-Z0-9\-._~!$&'()*+,;=:@%]+$/;
         setStringSlashCheck(regex.test(str));
@@ -137,6 +138,7 @@ const InputPage: React.FC = () => {
     };
 
     const addUrlMap = async () => {
+        setLoading2(true);
         const res = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/addMapping`, {
             realUrl: url,
             userUrl: customUrl,
@@ -146,6 +148,7 @@ const InputPage: React.FC = () => {
         } else {
             alert("The given URL already exists");
         }
+        setLoading2(false);
     };
 
     const handleRandomStringClick = (str: string) => {
@@ -169,13 +172,13 @@ const InputPage: React.FC = () => {
                 </div>
 
                 <div className="mb-6 inline-flex" >
-                <input
+                    <input
                         disabled={true}
                         className={` placeholder-black w-full p-4 border-2 rounded-lg transition-colors focus:outline-none focus:border-blue-500 ${validCustom ? "border-gray-300 bg-gray-50" : "border-blue-500"
                             }`}
                         placeholder={`${process.env.NEXT_PUBLIC_API_BASE_URL}/`}
                         type="text"
-                        
+
                     />
                     <input
                         disabled={mapped}
@@ -203,7 +206,7 @@ const InputPage: React.FC = () => {
                             </div>
                         )
                     }
-                    
+
 
                     {
                         !stringSlashCheck && (
@@ -219,35 +222,69 @@ const InputPage: React.FC = () => {
                             Check Availability
                         </button>
                     )}
+                    {loading && (
+                        <button
+                            className="mt-4 bg-blue-500 text-white w-full py-3 rounded-lg hover:bg-blue-600 transition-colors"
+                            onClick={checkCustomUrl}
+                        >
+                            <div className="text-center">
+                                <div role="status">
+                                    <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                    </svg>
+                                    <span className="sr-only">Loading...</span>
+                                </div>
+                            </div>
 
-                {validCustom && valid && (
-                    <button
-                    className="mt-4 bg-green-500 text-white w-full py-3 rounded-lg hover:bg-green-600 transition-colors"
-                    onClick={addUrlMap}
-                    >
-                        Add URL
-                    </button>
-                )}
+                        </button>
+                    )}
 
-                {mapped && (
-                    <div className="mt-6 flex flex-col items-center">
-                        <div className="flex items-center">
-                            <p className="text-blue-600 font-semibold text-lg">{`${process.env.NEXT_PUBLIC_API_BASE_URL}/${customUrl}`}</p>
-                            <button
-                                ref={clipboardRef}
-                                className="ml-4 text-gray-500 hover:text-gray-700"
-                                title="Copy to clipboard"
+                    {!loading2 && validCustom && valid && (
+                        <button
+                            className="mt-4 bg-green-500 text-white w-full py-3 rounded-lg hover:bg-green-600 transition-colors"
+                            onClick={addUrlMap}
+                        >
+                            Add URL
+                        </button>
+                    )}
+                    {loading2 && (
+                        <button
+                            className="mt-4 bg-green-500 text-white w-full py-3 rounded-lg hover:bg-green-600 transition-colors"
+                            onClick={addUrlMap}
+                        >
+                            <div className="text-center">
+                                <div role="status">
+                                    <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                    </svg>
+                                    <span className="sr-only">Loading...</span>
+                                </div>
+                            </div>
+
+                        </button>
+                    )}
+
+                    {mapped && (
+                        <div className="mt-6 flex flex-col items-center">
+                            <div className="flex items-center">
+                                <p className="text-blue-600 font-semibold text-lg">{`${process.env.NEXT_PUBLIC_API_BASE_URL}/${customUrl}`}</p>
+                                <button
+                                    ref={clipboardRef}
+                                    className="ml-4 text-gray-500 hover:text-gray-700"
+                                    title="Copy to clipboard"
                                 >
-                                <ClipboardIcon />
-                            </button>
-                            {isCopied && <span className="ml-2 text-green-600 text-sm">Copied!</span>}
+                                    <ClipboardIcon />
+                                </button>
+                                {isCopied && <span className="ml-2 text-green-600 text-sm">Copied!</span>}
+                            </div>
+                            <p className="text-gray-500 mt-4">Refresh the page to shorten another URL</p>
                         </div>
-                        <p className="text-gray-500 mt-4">Refresh the page to shorten another URL</p>
-                    </div>
-                )}
+                    )}
                 </div>
             </div>
-            <MyUrlsButton/>
+            <MyUrlsButton />
         </div>
     );
 };
